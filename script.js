9734171e383f024f0d03ffe5541ee7257d587a21
DanielW114 <158521428+DanielW114@users.noreply.github.com>
2024-05-16 21:19:48 +0000

    Day 3

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ let button1 = document.querySelector(".option-one");
 let screen1 = document.querySelector(".option-one-screen");
 let continue1 = document.querySelector(".continue-1");
 let ending1 = document.querySelector(".option-one-end");
-let restart1 = document.querySelector(".restart1")
+let restart1 = document.querySelector(".restart1");
 let button2 = document.querySelector(".option-two");
 let screen2 = document.querySelector(".option-two-screen");
 let continue2 = document.querySelector(".continue-2");
 let ending2 = document.querySelector(".option-two-end");
-let restart2 = document.querySelector(".restart2")
-
+let restart2 = document.querySelector(".restart2");
+let button3 = document.querySelector(".alt-path");
+let ending3 = document.querySelector(".alt-path-end");
+let restart3 = document.querySelector(".restart3")
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
 
@@ -46,6 +48,18 @@ restart1.addEventListener('click', function() {
 
 restart2.addEventListener('click', function() {
   ending2.style.display = "none"
+  opening.style.display = "contents"
+  title.innerHTML = "3 Little Pigs"
+});
+
+button3.addEventListener('mouseover', function() {
+  screen2.style.display = "none"
+  ending3.style.display = "contents"
+  title.innerHTML = "Alternate Ending"
+});
+
+restart3.addEventListener('click', function() {
+  ending3.style.display = "none"
   opening.style.display = "contents"
   title.innerHTML = "3 Little Pigs"
 });
